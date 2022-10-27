a2f7c56969595461b1a05a01d24cf3756e1d6a5d
Dmitrii.Kazantsev <dmitry.kazantsev@jetbrains.com>
2022-10-27 11:25:42 +0300

    Placeholder placed

---
+++ cs_con_net6/Processor.cs
@@ -0,0 +1,14 @@
+namespace cs_con_net6;
+
+public class Processor
+{
+    public static void CoreFuntionality()
+    {
+        
+    }
+
+    public static void HelperFunction(string handler)
+    {
+        
+    }
+}
