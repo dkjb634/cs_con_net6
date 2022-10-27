e1c8526231ae732fc36fa5cc4092fef9f0880b23
Dmitrii.Kazantsev <dmitry.kazantsev@jetbrains.com>
2022-10-27 11:10:43 +0300

    Update

--- cs_con_net6/Program.cs
+++ cs_con_net6/Program.cs
@@ -1,3 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+int a = 5;
+int b = 6;
+int rez = a + b;
+Console.WriteLine("Hello, World! " + rez);
