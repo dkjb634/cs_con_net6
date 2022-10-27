73564747b837a6fc05e20e35dd730588dd604f79
Dmitrii.Kazantsev <dmitry.kazantsev@jetbrains.com>
2022-10-27 11:29:24 +0300

    Dev1Implementation

--- cs_con_net6/Processor.cs
+++ cs_con_net6/Processor.cs
@@ -4,11 +4,19 @@ public class Processor
 {
     public static void CoreFuntionality()
     {
-        
+        Console.WriteLine("Welcome to our program. Would you like to be said welcome again? 1 - Yes, 2 - No");
+        int userRez = Convert.ToInt32(Console.ReadLine());
+        if (userRez == 1)
+        {
+            CoreFuntionality();
+        }
+
+        string bye = "GoodBye";
+        HelperFunction(bye);
     }
 
     public static void HelperFunction(string handler)
     {
-        
+        Console.WriteLine(handler);
     }
 }
--- cs_con_net6/Program.cs
+++ cs_con_net6/Program.cs
@@ -1,7 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-int a = 5;
-int b = 6;
-int rez = a + b;
-//comment
-Console.WriteLine("Hello, World! " + rez);
+using cs_con_net6;
+
+Processor.CoreFuntionality();
