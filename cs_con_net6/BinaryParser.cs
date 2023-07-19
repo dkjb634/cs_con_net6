a2f42426c31fbb53ade547591882be9551424508
dkjb634 <dmitry.kazantsev@jetbrains.com>
2023-07-19 10:35:50 +0300

    ee

---
+++ cs_con_net6/BinaryParser.cs
@@ -0,0 +1,28 @@
+﻿using System.Reflection.Metadata;
+
+namespace cs_con_net6;
+
+public class BinaryParser
+{
+    public FieldKing ParseField(ReadOnlySpan<char> header)
+    {
+        if (header.SequenceEqual("<==HEADER==>"))
+        {
+            return new FieldKing();
+        }
+
+        return new FieldKing();
+    }
+
+    private SequencePoint header;
+}
+
+public class FieldKing
+{
+    private BinaryParser Reference;
+
+    public FieldKing foo()
+    {
+        return Reference.ParseField(new ReadOnlySpan<char>(new[] { 'a', 'b', 'v' }));
+    }
+}
